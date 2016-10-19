5680641897592d84bf4a20c96bc7741094a10478
Nosagie Asaolu <nasaolu@middlebury.edu>
2016-10-19 17:20:23 -0400

    Added JSON Keys and working API calls in MarketSnapshotFragment

--- NSETrackerNigeria/app/src/main/java/com/nosagieapp/nsetracker/nsetrackernigeria/MarketSnapshotFragment.java
+++ NSETrackerNigeria/app/src/main/java/com/nosagieapp/nsetracker/nsetrackernigeria/MarketSnapshotFragment.java
@@ -1,11 +1,14 @@
 package com.nosagieapp.nsetracker.nsetrackernigeria;
 
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.method.ScrollingMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 /**
@@ -15,6 +18,14 @@ import android.view.ViewGroup;
  */
 public class MarketSnapshotFragment extends Fragment {
 
+    TextView test;
+
+    //JSON Keys
+    private final String ASI_KEY = "ASI";
+    private final String DEALS_KEY = "DEALS";
+    private final String VOLUME_KEY = "VOLUME";
+    private final String VALUE_KEY = "VALUE";
+    private final String MARKET_CAP_KEY = "CAP";
 
 
     public MarketSnapshotFragment() {
@@ -26,13 +37,36 @@ public class MarketSnapshotFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        new fetchMarketSnapshot().execute();
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_market_snapshot, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_market_snapshot, container, false);
+
+        test = (TextView)rootView.findViewById(R.id.mrktsnaptextviewtest);
+        test.setMovementMethod(new ScrollingMovementMethod());
+
+        return rootView;
+    }
+
+    private class fetchMarketSnapshot extends AsyncTask<String,Void,String>{
+        @Override
+        protected String doInBackground(String... params) {
+            String unparsedJSON = Fetcher.fetchMarketSnapshot();
+
+            return unparsedJSON.substring(4); //removes "null" at the beginning of resulting string
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            test.setText(s);
+        }
     }
 
 }
